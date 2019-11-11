a84f19ff814b797454fff55cd82f60e814d0e674
Robert <bibbycodes@gmail.com>
2019-11-11 16:13:47 +0000

    initial commit

---
+++ thermostat.js
@@ -0,0 +1,16 @@
+Greeting = {
+  hello: function(person) {
+    return `Hello ${person}`
+  }
+}
+
+function hiTimmy() {
+  return 'Hi Timmy!'
+}
+
+function hi(name) {
+
+}
+
+console.log(Greeting.hello("Robert"))
+console.log(hiTimmy())
